bbd7fd30667c2b3c5974194e5379ef16d7b1aaae
Merziv <44921219+Merziv@users.noreply.github.com>
2019-06-06 18:31:08 +0200

    DRX update v1

--- venv/main.py
+++ venv/main.py
@@ -7,6 +7,7 @@ from collections import Counter
 from numpy.random import permutation
 import math
 from functools import partial
+from tkinter.messagebox import showinfo
 
 c_index = 1
 c_tab = ["white"]
@@ -65,8 +66,16 @@ def shuffle2d(arr2d, rand=random):
 def create(numel):
     #utworz wstepna tablice
     global e_tab
+    global ro_tab
+    global prev_rdx_tab
+    global rdx_tab
+
     tab = np.zeros(numel * numel).reshape(numel, numel)
     e_tab = np.zeros(numel * numel).reshape(numel, numel)
+    ro_tab = np.zeros(numel * numel).reshape(numel, numel)
+    prev_rdx_tab = np.zeros(numel * numel).reshape(numel, numel)
+    rdx_tab = np.zeros(numel * numel).reshape(numel, numel)
+
     return tab
 
 def sc_create(numel):
@@ -162,7 +171,7 @@ def selected():
     if opt == "Homogenous":
         homo(tab, rows, cols)
     elif opt == "In range":
-        in_range(tab, rad)
+        in_range(tab, rad, amount)
     elif opt == "Manual choose":
         man(tab, numel)
     elif opt == "Random":
@@ -173,6 +182,10 @@ def selected():
 
 def homo(tab, a, b):
     #jednorodnie
+    if  a<1 or b<1 or a>len(tab) or b>len(tab):
+        a=3
+        b=3
+
     step1 = int(numel/a)+1
     step2 = int(numel/b)+1
 
@@ -192,7 +205,8 @@ def neighbors(mat, row, col, radius):
     #sasiedztwo w promieniu
     rows, cols = len(mat), len(mat[0])
     c = 0
-
+    if radius<1:
+        radius=1
     for i in range(row - radius, row + radius + 1):
         for j in range(col - radius, col + radius + 1):
             if 0 <= i < rows and 0 <= j < cols:
@@ -200,14 +214,29 @@ def neighbors(mat, row, col, radius):
                     c+=1
     return c
 
-def in_range(tab, rad):
+
+def popup_showinfo():
+    showinfo("Fail", "Amount was reduced")
+
+
+def in_range(tab, rad, amount):
     #w promieniu
+    cnt = 1
+    if rad<1 or rad>len(tab):
+        rad=1
     for i in permutation(numel):
         for j in permutation(numel):
             c = neighbors(tab, i, j, rad)
             if c==0:
+                cnt+=1
                 color='#{:02x}{:02x}{:02x}'.format(r(), r(), r())
                 OneSquare(can,scale*j,scale*i,scale*j+scale,scale*i+scale,color,i,j,tab)
+            if cnt>amount:
+                    break
+        if cnt > amount:
+                break
+    if cnt<=amount:
+        popup_showinfo()
 
 
 def man(tab, numel):
@@ -387,6 +416,10 @@ def growthRange(tab,N, radius):
     global sc_tab
     tab2 = tab.copy()
     rows, cols = len(tab), len(tab[0])
+
+    if radius<1 or radius>rows or radius>cols:
+        radius=1
+
     for i in range(N):
         for j in range(N):
             n_tab =[]
@@ -415,15 +448,27 @@ def energy():
     global tab
     global e_tab
     global numel
+    mode = print_mode.get()
+    per = periodic.get()
     N = numel
     rows, cols = len(tab), len(tab[0])
     for i in permutation(N):
         for j in permutation(N):
             n_tab = []
-            for x in range(i - 1, i + 2):
-                for y in range(j - 1, j + 2):
-                    if 0 <= x < rows and 0 <= y < cols and x!=y:
-                         n_tab.append(int(tab[x,y]))
+            if not per:
+                for x in range(i - 1, i + 2):
+                    for y in range(j - 1, j + 2):
+                        if 0 <= x < rows and 0 <= y < cols and x!=y:
+                             n_tab.append(int(tab[x,y]))
+            else:
+                n_tab.append(int(tab[i, (j - 1) % N]))
+                n_tab.append(int(tab[i, (j + 1) % N]))
+                n_tab.append(int(tab[(i - 1) % N, j]))
+                n_tab.append(int(tab[(i + 1) % N, j]))
+                n_tab.append(int(tab[(i - 1) % N, (j - 1) % N]))
+                n_tab.append(int(tab[(i - 1) % N, (j + 1) % N]))
+                n_tab.append(int(tab[(i + 1) % N, (j - 1) % N]))
+                n_tab.append(int(tab[(i + 1) % N, (j + 1) % N]))
             n_tab2 = n_tab.copy()
             c = neighbors(tab, i, j, radius=1)
             if c<=0:
@@ -447,7 +492,9 @@ def energy():
                 eAfter = len(n_tab2)
 
                 deltaE = eAfter - eBefore;
-                Kt = 0.6
+                Kt = float(entry_kt.get())
+                if Kt<0.1 or Kt>0.6:
+                    Kt=0.1
                 p = random.random();
                 expo = math.exp(-((deltaE) / (Kt)));
                 # print('xd')
@@ -462,7 +509,10 @@ def energy():
                     e_tab[i][j] = eAfter
                 else:
                     e_tab[i][j] = eBefore
-    prints(tab,numel)
+    if mode:
+        print_energy()
+    else:
+        prints(tab,numel)
 
 def ppt():
     global tab
@@ -474,7 +524,163 @@ def ppt():
             OneSquare(can, scale * j, scale * i, scale * j + scale, scale * i + scale, "white", i, j, tab)
     prints(tab,numel)
 
-#niestabilny fragment
+#############
+
+def calc_ro(A,B,t):
+    ro = (A / B) + (1 - A / B) * math.exp(-B * t)
+    return ro
+
+def drx_print(rdx_tab, ro_tab, numel, pCritical):
+    for i in range(numel):
+        for j in range(numel):
+            if rdx_tab[i][j] == 1:
+                can.create_rectangle(scale * j, scale * i, scale * j + scale, scale * i + scale, fill="black")
+            else:
+                nr = int( (ro_tab[i][j]/pCritical) * 255)
+                if nr <= 255:
+                    can.create_rectangle(scale * j, scale * i, scale * j + scale, scale * i + scale,
+                                     fill="#{:02x}0000".format(255-nr))
+                else:
+                    can.create_rectangle(scale * j, scale * i, scale * j + scale, scale * i + scale,
+                                     fill="white")
+
+def drx():
+    global ro_list
+    global numel
+    global prev_rdx_tab
+    global prev_ro_tab
+    global rdx_tab
+    global ro_tab
+    global tab
+
+    rows, cols = len(tab), len(tab[0])
+
+    A = 86710969050178.5
+    B = 9.41268203527779
+    dt = 0.001
+    tMax = 0.2
+    prevRo = 0
+
+    pCritical = 4215840142323.42 / (numel * numel)
+    #pCritical = 46842668.25
+
+    xd = 0
+
+    equalDistributionpercentage = 0.3
+    randomPackagePercentage = 0.3
+
+    for t in np.arange(0, tMax, dt):
+        # calculate ro
+        if t > 0:
+            prev_rdx_tab = tmp_rdx_tab.copy()
+            prev_ro_tab = ro_tab.copy()
+
+        tmp_rdx_tab = np.zeros(numel * numel).reshape(numel, numel)
+
+        ro = (A / B) + (1 - A / B) * math.exp(-B * t)
+        # print(ro)
+
+        if t == 0:
+            dRo = ro
+        else:
+            prevRo = ro_list[len(ro_list) - 1]
+            dRo = ro - prevRo
+
+        # dystrybucja
+        roPerCell = dRo / (numel * numel);
+
+        equalDistribution = roPerCell * equalDistributionpercentage;
+
+        randomDistribution = roPerCell * randomPackagePercentage;
+
+        [x + equalDistribution for x in ro_tab]
+
+        dRo *= (1.0 - equalDistributionpercentage)
+
+        iterationCount = int(dRo / randomDistribution)
+
+        for i in range(iterationCount):
+            p = random.random()
+
+            i = random.randint(0, numel - 1)
+            j = random.randint(0, numel - 1)
+
+            n_tab = []
+
+            for x in range(i - 1, i + 2):
+                for y in range(j - 1, j + 2):
+                    if 0 <= x < rows and 0 <= y < cols and x != y:
+                        n_tab.append(int(tab[x, y]))
+
+            k = tab[i][j]
+            while k in n_tab:
+                n_tab.remove(k)
+
+            bound = False
+
+            if len(n_tab) == 0:
+                prob = 0.2
+            else:
+                prob = 0.8
+                bound = True
+
+            if p <= prob:
+                ro_tab[i][j] += randomDistribution
+
+        #zarodkowanie
+        for i in range (numel):
+            for j in range (numel):
+                if rdx_tab[i][j] == 1:
+                    continue
+                #rekrystalizacja
+                if bound:
+                    if ro_tab[i][j] > pCritical:
+                        rdx_tab[i][j] = 1
+                        tmp_rdx_tab[i][j] = 1
+                        ro_tab[i][j] = 0
+                        continue
+
+                #rozrost
+                rcl_tab = []
+
+                for x in range(i - 1, i + 2):
+                    for y in range(j - 1, j + 2):
+                        if 0 <= x < rows and 0 <= y < cols and x != y:
+                            rcl_tab.append(int(prev_rdx_tab[x, y]))
+
+                if sum(rcl_tab) > 0:
+                    neigh_ro = []
+
+                    for x in range(i - 1, i + 2):
+                        for y in range(j - 1, j + 2):
+                            if 0 <= x < rows and 0 <= y < cols and x != y:
+                                neigh_ro.append(int(prev_ro_tab[x, y]))
+
+                    if ro_tab[i][j] > max(neigh_ro):
+                        rdx_tab[i][j] = 1
+                        tmp_rdx_tab[i][j] = 1
+                        ro_tab[i][j] = 0
+                        continue
+        #finito
+        ro_list.append(ro)
+    drx_print(rdx_tab,ro_tab,numel, pCritical)
+
+    f = open("results2.csv", "w+")
+
+    f.write("Time:; ro:\n")
+    for i in range(len(ro_list)):
+        f.write("{}; {}\n".format(dt*i,ro_list[i]))
+
+
+#all
+ro_sum = []
+prev_ro_tab = []
+ro_tab = []
+prev_rdx_tab = []
+rdx_tab = []
+
+ro_list = []
+
 r = lambda: random.randint(0,255)
 tab = np.array([])
 sc_tab = np.array([])
@@ -485,7 +691,7 @@ scale = 10
 cnt = 0
 
 mGui = Tk()
-mGui.geometry('600x765+500+25')
+mGui.geometry('710x765+500+25')
 mGui.title('Grains growth')
 mGui.resizable(True,True)
 
@@ -493,16 +699,11 @@ my_var = IntVar()
 size = StringVar()
 
 
-WIDTH, HEIGHT = 600, 600
+WIDTH, HEIGHT = 500, 500
 
 paused = True
-
 neigh = None
 
-# rb1 = Radiobutton(mGui, text='Homogenous', variable=my_var, value=30)
-# rb2 = Radiobutton(mGui, text='In range', variable=my_var, value=60)
-# rb3 = Radiobutton(mGui, text='Manual choose', variable=my_var, value=90)
-# rb4 = Radiobutton(mGui, text='Random', variable=my_var, value=225)
 
 OPTIONS = [
 "Homogenous",
@@ -511,24 +712,26 @@ OPTIONS = [
 "Random"
 ]
 
-lab_nuc = Label(text='\nGrain nucleation:\n').grid(row=0, sticky='W', padx=(3,0))
+lab_nuc = Label(text='\nGrain nucleation:').grid(row=0, sticky='W', padx=(2,0))
 
 my_var = StringVar(mGui)
 my_var.set(OPTIONS[0])
 
-dl1 = OptionMenu(mGui,my_var,*OPTIONS).grid(row=1, sticky='W', padx=(3,0))
+dl1 = OptionMenu(mGui,my_var,*OPTIONS).grid(row=1, sticky='W', padx=(2,0))
 
 but = Button(mGui,text='Show',command=selected)
 but1 = Button(mGui,text='Start',command=pause)
-but2 = Button(mGui,text='Energy',command=energy)
+but2 = Button(mGui,text='Monte Carlo',command=energy)
 but3 = Button(mGui,text='Print Energy',command=print_energy)
 but4 = Button(mGui,text='Print Normal',command=ppt)
+but5 = Button(mGui,text='DRX',command=drx)
 
-but.grid(row=1, sticky='E')
-but1.grid(row=2, sticky='E')
-but2.grid(row=3, sticky='E')
-but3.grid(row=4, sticky='E')
-but4.grid(row=5, sticky='E')
+but.grid(row=0, sticky='E')
+but1.grid(row=1, sticky='E')
+but2.grid(row=2, sticky='E')
+but3.grid(row=3, sticky='E')
+but4.grid(row=4, sticky='E')
+but5.grid(row=9, sticky='E')
 
 
 OPTIONS2 = [
@@ -539,27 +742,48 @@ OPTIONS2 = [
 "In radius"
 ]
 
-lab_neig = Label(text='\nNeighborhood:\n').grid(row=2, sticky='W', padx=(3,0))
+lab_neig = Label(text='\nNeighborhood:').grid(row=2, sticky='W', padx=(2,0))
 
 my_var2 = StringVar(mGui)
 my_var2.set(OPTIONS2[0])
 
-dl2 = OptionMenu(mGui,my_var2,*OPTIONS2).grid(row=3, sticky='W', padx=(3,0))
+dl2 = OptionMenu(mGui,my_var2,*OPTIONS2).grid(row=3, sticky='W', padx=(2,0))
 
 isRand = BooleanVar(mGui)
 isRand.set(False)
+
+periodic = BooleanVar()
+periodic.set(False)
+
+
+print_mode = BooleanVar()
+print_mode.set(False)
+
+iteration = IntVar()
+iteration.set(0)
+
+entry_kt = Entry(mGui)
+lab3 = Label(text='kT:').grid(row=5, column=0, sticky='E')
+entry_kt.insert(END, '0.1')
+entry_kt.grid(row=6, column=0, sticky='E')
+
 rb1 = Checkbutton(mGui, text='Random', variable=isRand)
-rb1.grid(row=4, sticky='W', padx=(3,0))
+rb1.grid(row=4, sticky='W', padx=(2,0), pady=(2.0))
+rb4 = Checkbutton(mGui, text='isPeriodic', variable=periodic)
+rb4.grid(row=8, sticky='W', padx=(2,0))
+rb5 = Checkbutton(mGui, text='Calculate in energy mode', variable=print_mode)
+rb5.grid(row=8, sticky='E', padx=(2,0))
+
 
 entry2 = Entry(mGui) #size
 
 side = IntVar(mGui)
 
 rb2 = Radiobutton(mGui, text='Left', variable=side, value="1")
-rb2.grid(row=5, sticky='W', padx=(10,0))
+rb2.grid(row=5, sticky='W', padx=(5,0))
 rb2.select()
 rb3 = Radiobutton(mGui, text='Right', variable=side, value="2")
-rb3.grid(row=6, sticky='W', padx=(10,0))
+rb3.grid(row=6, sticky='W', padx=(5,0))
 
 lab2 = Label(text='Size:').grid(row=6, column=0, sticky='N')
 entry2.insert(END, '50')
@@ -574,7 +798,7 @@ entry_r = Entry(mGui)
 entry_i.insert(END, '5')
 entry_j.insert(END, '5')
 entry_rnd.insert(END, '10')
-entry_r.insert(END, '5')
+entry_r.insert(END, '7')
 
 
 lab_r = Label(text='Radius:').grid(row=0, column=0, sticky='N')
@@ -589,9 +813,32 @@ entry_j.grid(row=3, column=0, sticky='N')
 lab_j = Label(text='Amount:').grid(row=4, column=0, sticky='N')
 entry_rnd.grid(row=5, column=0, sticky='N')
 
+entry_A = Entry(mGui)
+lab3 = Label(text='A:').grid(row=0, column=1)
+entry_A.insert(END, '86710969050178.5')
+entry_A.grid(row=1, column=1)
+
+
+entry_B = Entry(mGui)
+lab3 = Label(text='B:').grid(row=2, column=1)
+entry_B.insert(END, '9.41268203527779')
+entry_B.grid(row=3, column=1)
+
+
+entry_dt = Entry(mGui)
+lab3 = Label(text='dt:').grid(row=4, column=1)
+entry_dt.insert(END, '0.001')
+entry_dt.grid(row=5, column=1)
+
+
+entry_tmax = Entry(mGui)
+lab3 = Label(text='tMax:').grid(row=6, column=1)
+entry_tmax.insert(END, '0.2')
+entry_tmax.grid(row=7, column=1)
+
 
 can = Canvas(mGui, width=WIDTH, height=HEIGHT, bg="#ffffff")
-can.grid(row=8, column=0)
+can.grid(row=10, column=0)
 
 
 mGui.mainloop()
